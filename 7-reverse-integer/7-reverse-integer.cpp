6fbc4998b94bcc65f26011cac2f18c5322b0fda1
Ayush Sinha <54765074+Ayushsinhahaha@users.noreply.github.com>
2022-06-03 19:33:16 +0530

    Time: 7 ms (17.14%), Space: 5.8 MB (77.35%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rem=0,rev=0;
+        while(x!=0)
+        {
+            rem=x%10;
+            if((rev<INT_MIN/10)||(rev>INT_MAX/10))
+            {
+                return 0;
+            }
+            rev=rev*10+rem;
+            x/=10;
+        }
+        return rev;
+    }
+};
