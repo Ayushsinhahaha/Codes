71efe44f3e850e2fb527495634c1462d0f678598
Ayush Sinha <54765074+Ayushsinhahaha@users.noreply.github.com>
2022-08-20 02:17:39 +0530

    Time: 4 ms (41.74%), Space: 8.4 MB (42.50%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -14,9 +14,8 @@ public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int>preorder;
         if(root==NULL)
-        {
             return preorder;
-        }
+        
         stack<TreeNode*>st;
         st.push(root);
         while(!st.empty())
@@ -25,10 +24,10 @@ public:
             st.pop();
             preorder.push_back(root->val);
             if(root->right!=NULL)
-            st.push(root->right);
+                st.push(root->right);
             if(root->left!=NULL)
-            st.push(root->left);
+                st.push(root->left);
         }
-        return preorder;
+    return preorder;
     }
 };
